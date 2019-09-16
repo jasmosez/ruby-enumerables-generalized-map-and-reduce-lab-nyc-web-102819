4b26103bbf0645ded13c24bb5dcd71b8842b30bf
James Schaffer <jas.schaffer@gmail.com>
2019-09-16 21:21:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,9 @@ def reduce(source_array, starting_point = 0)
   result = starting_point
   i = 0
   while i < source_array.length do
+    puts "#{result} is result before update"
     result = yield(source_array[i], result)
+    puts "#{result} is result after update"
     i += 1
   end
   return result
